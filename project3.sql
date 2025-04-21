0f6273ca956599e353064caea0e476f46d2f8b3a
DaltonGorham <daltongorham@gmail.com>
2025-04-20 21:59:51 -0500

    fixed inserts and table values to hold enough characters

--- project3.sql
+++ project3.sql
@@ -16,107 +16,107 @@ DROP TABLE movie_streaming_service CASCADE CONSTRAINTS;
 DROP TABLE movie_genre CASCADE CONSTRAINTS;
 
 CREATE TABLE director (
-name VARCHAR2 (15) CONSTRAINT director_name_pk PRIMARY KEY,
+name VARCHAR2 (30) CONSTRAINT director_name_pk PRIMARY KEY,
 description VARCHAR2 (5000),
 dob DATE,
 dod DATE
 );
 
 CREATE TABLE writer (
-name VARCHAR2 (15) CONSTRAINT writer_name_pk PRIMARY KEY,
+name VARCHAR2 (30) CONSTRAINT writer_name_pk PRIMARY KEY,
 description VARCHAR2 (5000),
 dob DATE,
 dod DATE
 );
 
 CREATE TABLE actor (
-name VARCHAR2 (15) CONSTRAINT actor_name_pk PRIMARY KEY,
+name VARCHAR2 (30) CONSTRAINT actor_name_pk PRIMARY KEY,
 description VARCHAR2 (5000),
 dob DATE,
 dod DATE
 );
 
 CREATE TABLE directs_tv_show (
-tv_show_id NUMBER (10),
-director_name VARCHAR2 (15),
+tv_show_id VARCHAR2 (20),
+director_name VARCHAR2 (30),
 CONSTRAINT directs_tv_id_dir_name_pk PRIMARY KEY (tv_show_id, director_name)
 );
 
 CREATE TABLE acts_tv_show (
-tv_show_id NUMBER (10),
-actor_name VARCHAR2 (15),
+tv_show_id VARCHAR2 (20),
+actor_name VARCHAR2 (30),
 CONSTRAINT acts_tv_id_act_name_pk PRIMARY KEY (tv_show_id, actor_name)
 );
 
 CREATE TABLE writes_tv_show (
-tv_show_id NUMBER (10),
-writer_name VARCHAR2 (15),
+tv_show_id VARCHAR2 (20),
+writer_name VARCHAR2 (30),
 CONSTRAINT writes_tv_id_wr_name_pk PRIMARY KEY (tv_show_id, writer_name)
 );
 
 CREATE TABLE directs_movie (
-movie_id NUMBER (10),
-director_name VARCHAR2 (15),
+movie_id VARCHAR2 (20),
+director_name VARCHAR2 (30),
 CONSTRAINT directs_id_dir_name_pk PRIMARY KEY (movie_id, director_name)
 );
 
 CREATE TABLE acts_movie (
-movie_id NUMBER (10),
-actor_name VARCHAR2 (15),
+movie_id VARCHAR2 (20),
+actor_name VARCHAR2 (30),
 CONSTRAINT acts_id_act_name_pk PRIMARY KEY (movie_id, actor_name)
 );
 
 CREATE TABLE writes_movie (
-movie_id NUMBER (10),
-writer_name VARCHAR2 (15),
+movie_id VARCHAR2 (20),
+writer_name VARCHAR2 (30),
 CONSTRAINT writes_id_wri_name_pk PRIMARY KEY (movie_id, writer_name)
 );
 
 CREATE TABLE episode (
-tv_show_id NUMBER (10),
+tv_show_id VARCHAR2 (20),
 season_number NUMBER (2),
 episode_number NUMBER (2),
 name VARCHAR2 (20),
 length NUMBER (4),
-description VARCHAR2 (50),
+description VARCHAR2 (1000),
 initial_air_date DATE,
 CONSTRAINT episode_id_sn_en_pk PRIMARY KEY (tv_show_id, season_number, episode_number)
 );
 
 CREATE TABLE tv_show (
-id NUMBER (10) CONSTRAINT tv_show_id_pk PRIMARY KEY,
+id VARCHAR2 (20) CONSTRAINT tv_show_id_pk PRIMARY KEY,
 description VARCHAR2 (5000),
-tv_show_name VARCHAR2 (20)
+tv_show_name VARCHAR2 (30)
 );
 
 CREATE TABLE tv_show_streaming_service (
-tv_show_id NUMBER (10),
+tv_show_id VARCHAR2 (20),
 streaming_service VARCHAR (15),
 CONSTRAINT tv_id_ss_pk PRIMARY KEY (tv_show_id, streaming_service)
 );
 
 CREATE TABLE tv_show_genre (
-tv_show_id NUMBER (10),
+tv_show_id VARCHAR2 (20),
 genre VARCHAR (15),
 CONSTRAINT tv_id_genre_pk PRIMARY KEY (tv_show_id, genre)
 );
 
 CREATE TABLE movie (
-id NUMBER (10) CONSTRAINT movie_id_pk PRIMARY KEY,
-movie_name VARCHAR2 (20),
+id VARCHAR2 (20) CONSTRAINT movie_id_pk PRIMARY KEY,
+movie_name VARCHAR2 (30),
 description VARCHAR2 (5000),
 duration NUMBER(4),
 rating NUMBER(2,1)
 );
 
 CREATE TABLE movie_streaming_service (
-movie_id NUMBER (10),
+movie_id VARCHAR2 (20),
 streaming_service VARCHAR (15),
 CONSTRAINT movie_id_ss_pk PRIMARY KEY (movie_id, streaming_service)
 );
 
 CREATE TABLE movie_genre (
-movie_id NUMBER (10),
+movie_id VARCHAR2 (20),
 genre VARCHAR (15),
 CONSTRAINT movie_id_genre_pk PRIMARY KEY (movie_id, genre)
 );
@@ -156,7 +156,7 @@ REFERENCES movie(id);
 
 ALTER TABLE tv_show_genre
 ADD CONSTRAINT tv_show_genre_id_fk FOREIGN KEY (tv_show_id)
-REFERENCES movie(id);
+REFERENCES tv_show(id);
 
 ALTER TABLE tv_show_streaming_service
 ADD CONSTRAINT tv_show_id_fk FOREIGN KEY (tv_show_id)
@@ -168,7 +168,7 @@ REFERENCES tv_show(id);
 
 insert into director (name, description, dob, dod)
 values ('Frank Darabont', 'Three-time Oscar nominee Frank Darabont was born in a refugee camp in 1959 in Montbeliard, France, the son of Hungarian parents who had fled Budapest during the failed 1956 Hungarian revolution. Brought to America as an infant, he settled with his family in Los Angeles and attended Hollywood High School. His first job in movies was as a production assistant on the 1981 low-budget film, Hell Night (1981), starring Linda Blair. He spent the next six years working in the art department as a set dresser and in set construction while struggling to establish himself as a writer. His first produced writing credit (shared) was on the 1987 film, A Nightmare on Elm Street 3: Dream Warriors (1987), directed by Chuck Russell. Darabont is one of only six filmmakers in history with the unique distinction of having his first two feature films receive nominations for the Best Picture Academy Award: 1994s The Shawshank Redemption (1994) (with a total of seven nominations) and 1999s The Green Mile (1999) (four nominations). Darabont himself collected Oscar nominations for Best Adapted Screenplay for each film (both based on works by Stephen King), as well as nominations for both films fro
-m the Directors Guild of America, and a nomination from the Writers Guild of America for The Shawshank Redemption (1994).', '1959-01-28', NULL);
+m the Directors Guild of America, and a nomination from the Writers Guild of America for The Shawshank Redemption (1994).', TO_DATE('1959-01-28', 'YYYY-MM-DD'), NULL);
 insert into director (name, description, dob, dod)
 values ('Francis Ford Coppola', 'Francis Ford Coppola was born in 1939 in Detroit, Michigan, but grew up in a New York suburb in a creative, supportive Italian-American 
 family. His father, Carmine Coppola, was a composer and musician. His mother, Italia Coppola (née Pennino), had been an actress. Francis Ford Coppola graduated with a 
@@ -187,11 +187,11 @@ oppola Oscars as a producer, director and writer. Coppola then began work on his
 Joseph Conrad s Heart of Darkness (1993). Released in 1979, the acclaimed film won a Golden Palm Award at the Cannes Film Festival, and two Academy Awards. Also that 
 year, Coppola executive produced the hit The Black Stallion (1979). With George Lucas, Coppola executive produced Kagemusha: The Shadow Warrior (1980), directed by Akira 
 Kurosawa, and Mishima: A Life in Four Chapters (1985), directed by Paul Schrader and based on the life and writings of Yukio Mishima. Coppola also executive produced 
-such films as The Escape Artist (1982), Hammett (1982) The Black Stallion Returns (1983), Barfly (1987), Wind (1992), The Secret Garden (1993), etc.', '1939-04-07', NULL);
+such films as The Escape Artist (1982), Hammett (1982) The Black Stallion Returns (1983), Barfly (1987), Wind (1992), The Secret Garden (1993), etc.', TO_DATE('1939-04-07', 'YYYY-MM-DD'), NULL);
 insert into director (name, description, dob, dod)
 values ('Jared Hess','Jared Lawrence Hess and Jerusha Elizabeth Hess (née Demke; born May 12, 1980) are husband-and-wife American filmmakers best known for their work on Napoleon Dynamite (2004), Nacho Libre (2006) and Gentlemen Broncos (2009), all of which they co-wrote and which were directed by Jared (Nacho Libre was co-written with Mike White). They 
 also produced music videos for The Postal Services third single, "We Will Become Silhouettes", and 
-The Killers Christmas charity single "Boots".', '1979-07-18', NULL);
+The Killers Christmas charity single "Boots".', TO_DATE('1979-07-18', 'YYYY-MM-DD'), NULL);
 insert into director (name, description, dob, dod)
 values ('Robert Zemeckis', 'A whiz-kid with special effects, Robert is from the Spielberg camp of film-making 
 (Steven Spielberg produced many of his films). Usually working with writing partner Bob Gale, Roberts earlier 
@@ -199,13 +199,13 @@ films show he has a talent for zany comedy (Romancing the Stone (1984), 1941 (19
 (Who Framed Roger Rabbit (1988) and Back to the Future (1985)). His later films have become more serious, with 
 the hugely successful Tom Hanks vehicle Forrest Gump (1994) and the Jodie Foster film Contact (1997), both 
 critically acclaimed movies. Again, these films incorporate stunning effects. Robert has proved 
-he can work a serious story around great effects.', '1951-05-14', NULL);
+he can work a serious story around great effects.', TO_DATE('1951-05-14', 'YYYY-MM-DD'), NULL);
 insert into director (name, description, dob, dod)
 values ('Christopher Nolan', 'Best known for his cerebral, often nonlinear, storytelling, acclaimed Academy Award winner writer/director/producer Sir Christopher Nolan CBE was born in London, England. Over the course of more than 25 years of filmmaking, Nolan has gone from low-budget independent films to working on some of the biggest blockbusters ever made and became one of the most celebrated filmmakers of modern cinema.
 At 7 years old, Nolan began making short films with his father s Super-8 camera. While studying English Literature at University College London, he shot 16-millimeter films at U.C.L. s film society, where he learned the guerrilla techniques he would later use to make his first feature, Following (1998), on a budget of around $6,000. The noir thriller was recognized at a number of international film festivals prior to its theatrical release and gained Nolan enough credibility that he was able to gather substantial financing for his next film.
 Nolan s second film was Memento (2000), which he directed from his own screenplay based on a short story by his brother Jonathan Nolan. Starring Guy Pearce, the film brought Nolan numerous honors, including Academy Award and Golden Globe Award nominations for Best Original Screenplay. Nolan went on to direct the critically acclaimed psychological thriller, Insomnia (2002), starring Al Pacino, Robin Williams and Hilary Swank.
 The turning point in Nolan s career occurred when he was awarded the chance to revive the Batman franchise in 2005. In Batman Begins (2005), Nolan brought a level of gravitas back to the iconic hero, and his gritty, modern interpretation was greeted with praise from fans and critics alike. Before moving on to a Batman sequel, Nolan directed, co-wrote, and produced the mystery thriller The Prestige (2006), starring Christian Bale and Hugh Jackman as magicians whose obsessive rivalry leads to tragedy and murder.
-', '1970-07-30', NULL);
+', TO_DATE('1970-07-30', 'YYYY-MM-DD'), NULL);
 
 insert into movie (id, movie_name, description, duration, rating)
 values ('tt0111161', 'The Shawshank Redemption', 'A banker convicted of uxoricide forms a friendship over a quarter century with a hardened convict, 
